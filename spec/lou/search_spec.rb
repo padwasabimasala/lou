f7cfef6f8076d8be6142ae7309890c7c72ae5be7
Matthew Thorley <padwasabimasala on gmail>
2013-07-26 13:06:58 -0600

    better spec for virtual attributes

--- spec/lou/search_spec.rb
+++ spec/lou/search_spec.rb
@@ -5,7 +5,7 @@ describe Lou::Search do
     described_class.should be_kind_of Class
   end
 
-  context "with virtual attributes and filter (selectors and joins) limit and order" do
+  context "with with kitchen sink (virtual attributes, filter, orders, and limit)" do
     let(:query_string) { "filter=last_name:eq=\"Juan de Marco\"+category_id:in=1,2,3+company_id:eq=77+employee_id:in=4,5&limit=10&order=id:desc" }
     let(:search) { Lou::Search.new(query_string, options) }
     let(:options) {{ 
@@ -41,31 +41,32 @@ describe Lou::Search do
     end
   end
 
-  context "with no virtual attributes filter (selectors but no joins) limit and order" do
-    let(:query_string) { "filter=last_name:eq=\"Juan de Marco\"+category_id:in=1,2,3&limit=10&order=id:desc" }
+  context "with with virtual attributes in the options" do
     let(:search) { Lou::Search.new(query_string, options) }
-    let(:options) {{ }}
+    let(:options) {{ 
+      virtual_attributes: {
+        company_id: { joins: :employees },
+        employee_id: { joins: :employees } 
+      }
+    }}
 
-    it "has a limit of 10" do
-      search.limit.should be 10
+    context "with virtual attributes in the query string" do
+      let(:query_string) { "filter=last_name:eq=\"Juan de Marco\"+company_id:eq=77+employee_id:in=4,5" }
+
+      it "has the correct joins" do
+        search.joins.size.should eq 1
+        search.joins[:employees].should =~ [
+          { attribute: :company_id, operator: :eq, value: '77' },
+          { attribute: :employee_id, operator: :in, value: ['4', '5'] }]
+      end
     end
 
-    it "has an order_by of id" do
-      search.order_by.should eq :id
-    end
+    context "with no virtual attributes in the query string" do
+      let(:query_string) { "filter=last_name:eq=\"Juan de Marco\"" }
 
-    it "has an order_direction of desc" do
-      search.order_direction.should eq :desc
-    end
-
-    it "has the correct selectors" do
-      search.selectors.size.should eq 2
-      search.selectors[:last_name].should eq({ operator: :eq, value: 'Juan de Marco' })
-      search.selectors[:category_id].should eq ({ operator: :in, value: ['1', '2', '3'] })
-    end
-
-    it "has the correct joins" do
-      search.joins.size.should eq 0
+      it "has the correct joins" do
+        search.joins.size.should eq 0
+      end
     end
   end
 
